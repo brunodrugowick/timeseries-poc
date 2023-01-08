fc8b1470ea1ffe111d24ade159b982966d207320
Bruno Drugowick <brunodrugowick@users.noreply.github.com>
2023-01-08 17:43:21 -0300

    Hibernate 6 "fixes" ?!

--- src/main/java/dev/drugowick/timeseriespoc/domain/entity/Snapshot.java
+++ src/main/java/dev/drugowick/timeseriespoc/domain/entity/Snapshot.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class Snapshot {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
 
     private String description;
